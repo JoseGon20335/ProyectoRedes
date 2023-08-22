401c65cc7dd590ae05145aacae117e98244d4af9
Carol Arevalo <gon20335@uvg.edu.gt>
2023-08-22 17:43:33 -0600

    contacts

---
+++ README.md
@@ -0,0 +1,14 @@
+Administración de cuentas
+[] Registrar una nueva cuenta en el servidor
+[X] Iniciar sesión con una cuenta
+[] Cerrar sesión con una cuenta
+[] Eliminar la cuenta del servidor
+Comunicación
+[] Mostrar todos los usuarios/contactos y su estado
+[] Agregar un usuario a los contactos
+[] Mostrar detalles de contacto de un usuario
+[] Comunicación 1 a 1 con cualquier usuario/contacto
+[] Participar en conversaciones grupales
+[] Definir mensaje de presencia
+[] Enviar/recibir notificaciones
+[] Enviar/recibir archivos
--- app/proyecto-redes/src/main/java/bmt/App.java
+++ app/proyecto-redes/src/main/java/bmt/App.java
@@ -1,24 +1,48 @@
 package bmt;
 
 import java.io.IOException;
-import java.util.Scanner;
 
 import org.jivesoftware.smack.AbstractXMPPConnection;
 import org.jivesoftware.smack.ConnectionConfiguration;
 import org.jivesoftware.smack.SmackException;
 import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.SmackException.NoResponseException;
+import org.jivesoftware.smack.SmackException.NotConnectedException;
+import org.jivesoftware.smack.SmackException.NotLoggedInException;
+import org.jivesoftware.smack.XMPPException.XMPPErrorException;
 import org.jivesoftware.smack.chat2.Chat;
 import org.jivesoftware.smack.chat2.ChatManager;
 import org.jivesoftware.smack.chat2.IncomingChatMessageListener;
 import org.jivesoftware.smack.packet.Message;
+import org.jivesoftware.smack.roster.Roster;
+import org.jivesoftware.smack.roster.RosterEntry;
+import org.jivesoftware.smack.roster.RosterListener;
 import org.jivesoftware.smack.tcp.XMPPTCPConnection;
 import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration;
 import org.jivesoftware.smackx.iqregister.AccountManager;
+import org.jxmpp.jid.BareJid;
 import org.jxmpp.jid.EntityBareJid;
 import org.jxmpp.jid.impl.JidCreate;
 import org.jxmpp.stringprep.XmppStringprepException;
 import org.minidns.record.A;
 import org.jxmpp.jid.parts.Localpart;
+import org.jivesoftware.smack.MessageListener;
+import org.jivesoftware.smack.packet.Presence;
+import org.jivesoftware.smackx.disco.ServiceDiscoveryManager;
+import org.jivesoftware.smackx.filetransfer.FileTransferManager;
+import org.jivesoftware.smackx.filetransfer.OutgoingFileTransfer;
+import org.jivesoftware.smackx.muc.MucEnterConfiguration;
+import org.jivesoftware.smackx.muc.MultiUserChat;
+import org.jivesoftware.smackx.muc.MultiUserChatManager;
+import org.jivesoftware.smackx.muc.MultiUserChatException.MucNotJoinedException;
+import org.jivesoftware.smackx.muc.MultiUserChatException.NotAMucServiceException;
+import org.jivesoftware.smackx.vcardtemp.VCardManager;
+import org.jivesoftware.smackx.vcardtemp.packet.VCard;
+import org.jxmpp.jid.EntityFullJid;
+import org.jxmpp.jid.Jid;
+import org.jxmpp.jid.parts.Resourcepart;
+import java.io.File;
+import java.util.*;
 
 public class App {
     static String nameToRefer = "";
@@ -105,7 +129,7 @@ public class App {
         boolean flag = false;
         System.out.print("Enter your username: ");
         String username = scanner.nextLine();
-        // username = username + "@alumchat.xyz";
+        username = username + "@alumchat.xyz";
 
         System.out.print("Enter your password: ");
         String password = scanner.nextLine();
@@ -163,36 +187,50 @@ public class App {
 
         System.out.println("Connected " + nameToRefer + " to the chat");
 
-        ChatManager chatManager = ChatManager.getInstanceFor(connection);
-        chatManager.addIncomingListener(new IncomingChatMessageListener() {
-            @Override
-            public void newIncomingMessage(EntityBareJid from, Message message, Chat chat) {
-                System.out.println("New message from " + from + ": " + message.getBody());
-            }
-        });
+        // ChatManager chatManager = ChatManager.getInstanceFor(connection);
+        // chatManager.addIncomingListener(new IncomingChatMessageListener() {
+        // @Override
+        // public void newIncomingMessage(EntityBareJid from, Message message, Chat
+        // chat) {
+        // System.out.println("New message from " + from + ": " + message.getBody());
+        // }
+        // });
 
         do {
             System.out.println("Welcome to chat undifined name we are not creeative enough to think of a name");
             System.out.println("1. Chat with someone");
             System.out.println("2. Chat in a group chat");
-            System.out.println("3. Exit");
+            System.out.println("3. Mostrar todos los usuarios");
+            System.out.println("4. Agregar un usuario a los contactos");
+            System.out.println("5. Detalles de un usuario");
+            System.out.println("6. Cambiar estado");
+            System.out.println("7. Cerrar sesion");
             System.out.print("Enter your choice: ");
             choice = scanner.nextInt();
 
             switch (choice) {
                 case 1:
-
+                    System.out.println("Chatear con alguien");
                     messageSender(connection);
                     break;
                 case 2:
-                    System.out.println("Sing in...");
-                    // XMPPTCPConnectionConfiguration config = signIn();
-
-                    // AbstractXMPPConnection connection = new XMPPTCPConnection(config);
-                    // connection.connect(); // Establishes a connection to the server
-                    // connection.login(); // Logs in
+                    System.out.println("Chatear en un grupo");
+                    messageSenderGroup(connection);
                     break;
                 case 3:
+                    System.out.println("Mostar contactos");
+                    break;
+                case 4:
+                    System.out.println("Agregar un usuario a los contactos");
+                    addContact(connection);
+                    break;
+                case 5:
+                    System.out.println("Detalles de un usuario");
+                    break;
+                case 6:
+                    System.out.println("Cambiar estado");
+                    break;
+                case 7:
                     System.out.println("Exiting the program.");
                     break;
                 default:
@@ -219,6 +257,76 @@ public class App {
         Chat chat = chatManager.chatWith(jid);
 
         chat.send(message);
+    }
+
+    public static void messageSenderGroup(AbstractXMPPConnection connection)
+            throws SmackException, IOException, XMPPException, InterruptedException {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the username of the destinatary: ");
+        String sendTo = scanner.nextLine();
+        sendTo = sendTo + "@alumchat.xyz";
+
+        System.out.print("Enter your message: ");
+        String message = scanner.nextLine();
+
+        ChatManager chatManager = ChatManager.getInstanceFor(connection);
+        EntityBareJid jid = JidCreate.entityBareFrom(sendTo);
+        Chat chat = chatManager.chatWith(jid);
+
+        chat.send(message);
+    }
+
+    public static void addContact(AbstractXMPPConnection connection)
+            throws NotLoggedInException, NoResponseException, XMPPErrorException,
+            NotConnectedException, InterruptedException, XmppStringprepException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el nombre del contacto para agregar");
+        String targetJID = sc.nextLine();
+        Roster roster = Roster.getInstanceFor(connection);
+        BareJid jid = JidCreate.bareFrom(targetJID + "@alumchat.xyz");
+
+        System.out.println("Ingrese su user");
+        final String userNameForContact = sc.nextLine();
+
+        final Roster finalRoster = roster; // Declare a final reference to the roster
+
+        finalRoster.addRosterListener(new RosterListener() {
+            @Override
+            public void presenceChanged(Presence presence) {
+                if (presence.getType() == Presence.Type.subscribe) {
+                    BareJid fromJID = presence.getFrom().asBareJid();
+
+                    try {
+                        // Aceptar el request using finalRoster
+                        finalRoster.createItemAndRequestSubscription(fromJID, userNameForContact, null);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void entriesAdded(Collection<Jid> addresses) {
+                // TODO Auto-generated method stub
+                throw new UnsupportedOperationException("Unimplemented method 'entriesAdded'");
+            }
+
+            @Override
+            public void entriesUpdated(Collection<Jid> addresses) {
+                // TODO Auto-generated method stub
+                throw new UnsupportedOperationException("Unimplemented method 'entriesUpdated'");
+            }
+
+            @Override
+            public void entriesDeleted(Collection<Jid> addresses) {
+                // TODO Auto-generated method stub
+                throw new UnsupportedOperationException("Unimplemented method 'entriesDeleted'");
+            }
+        });
+
+        // Send subscription request
+        finalRoster.createItemAndRequestSubscription(jid, targetJID, null);
     }
 
 }
